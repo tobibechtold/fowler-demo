b2d39f92a6a5d97b119d65537dd94cf676d0b02f
Tobias Bechtold <tobi.bechtold@gmail.com>
2014-05-25 18:21:19 +0200

    small cleanups...

--- src/Movie.java
+++ src/Movie.java
@@ -5,34 +5,42 @@ public class Movie {
     private String name;
     private int priceCode;
     private Price price;
+    
     public Movie(String newtitle, int newpriceCode) {
         name = newtitle;
         setPriceCode(priceCode);
     }
+    
     public int getPriceCode() {
         return price.getPriceCode();
     }
+    
     public void setPriceCode(int arg) {
     	switch (arg) {
-    	case REGULAR:
-    	price = new RegularPrice();
-    	break;
-    	case CHILDRENS:
-    	price = new ChildrensPrice();
-    	break;
-    	case NEW_RELEASE:
-    	price = new NewReleasePrice();
-    	break;
-    	default:
-    	throw new IllegalArgumentException("Incorrect Price Code");
+    		case REGULAR:
+    			price = new RegularPrice();
+    			break;
+    		case CHILDRENS:
+    			price = new ChildrensPrice();
+    			break;
+    		case NEW_RELEASE:
+    			price = new NewReleasePrice();
+    			break;
+    		default:
+    			throw new IllegalArgumentException("Incorrect Price Code");
     	}
-    	}
+    }
+    
     public String getTitle (){
         return name;
     };
     
 	
-	int getFrequentRenterPoints(int daysRented) {
+	public int getFrequentRenterPoints(int daysRented) {
 		return price.getFrequentRenterPoints(daysRented);
+	}
+	
+	public double getCharge(int daysRented) {
+		return price.getCharge(daysRented);
 	}
 }
