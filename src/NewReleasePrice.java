0c659cc13391b5a62b866108eb15438f3774491f
Tobias Bechtold <tobi.bechtold@gmail.com>
2014-05-25 18:16:32 +0200

    http://refactoring.com/catalog/replaceConditionalWithPolymorphism.html and adding Price Classes

---
+++ src/NewReleasePrice.java
@@ -0,0 +1,16 @@
+
+public class NewReleasePrice extends Price {
+
+	@Override
+	int getPriceCode() {
+
+		return Movie.NEW_RELEASE;
+	}
+
+	@Override
+	double getCharge(int daysRented) {
+
+		return daysRented * 3;
+	}
+
+}
--- src/Price.java
+++ src/Price.java
@@ -1,20 +1,7 @@
 public abstract class Price {
 
 	abstract int getPriceCode();
-	}
-	class ChildrensPrice extends Price {
-	int getPriceCode() {
-	return Movie.CHILDRENS;
-	}
-	}
-	class NewReleasePrice extends Price {
-	int getPriceCode() {
-	return Movie.NEW_RELEASE;
-	}
-	}
-	class RegularPrice extends Price {
-	int getPriceCode() {
-	return Movie.REGULAR;
-	}
+	
+	abstract double getCharge(int daysRented);
 	
 }
