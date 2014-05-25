4365ba8102344beaaaa895b790a9b738429335a3
Tobias Bechtold <tobi.bechtold@gmail.com>
2014-05-25 18:10:54 +0200

    Abstract Klasse Price implementiert

--- src/Movie.java
+++ src/Movie.java
@@ -4,17 +4,29 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String name;
     private int priceCode;
-    
+    private Price price;
     public Movie(String newtitle, int newpriceCode) {
         name = newtitle;
         setPriceCode(priceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
-    }
+    	switch (arg) {
+    	case REGULAR:
+    	price = new RegularPrice();
+    	break;
+    	case CHILDRENS:
+    	price = new ChildrensPrice();
+    	break;
+    	case NEW_RELEASE:
+    	price = new NewReleasePrice();
+    	break;
+    	default:
+    	throw new IllegalArgumentException("Incorrect Price Code");
+    	}
+    	}
     public String getTitle (){
         return name;
     };
---
+++ src/Price.java
@@ -0,0 +1,20 @@
+public abstract class Price {
+
+	abstract int getPriceCode();
+	}
+	class ChildrensPrice extends Price {
+	int getPriceCode() {
+	return Movie.CHILDRENS;
+	}
+	}
+	class NewReleasePrice extends Price {
+	int getPriceCode() {
+	return Movie.NEW_RELEASE;
+	}
+	}
+	class RegularPrice extends Price {
+	int getPriceCode() {
+	return Movie.REGULAR;
+	}
+	
+}
